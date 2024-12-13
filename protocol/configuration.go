f01e358fce76b120146a7c8be49a9c6b2de045f5
İbrahim Uysal <uysalibov@gmail.com>
2024-12-13 14:37:17 +0300

    feat: handle configuration information

--- protocol/configuration.go
+++ protocol/configuration.go
@@ -7,19 +7,35 @@ import (
 
 func HandleConfiguration(conn *connection, packet *incommingPacket) {
 	switch packet.id {
+	case 0x00:
+		locale := packet.ReadString()
+		viewDistance, _ := packet.ReadBytes(1)
+		chatMode, _ := packet.ReadVarInt()
+		charColors, _ := packet.ReadBoolean()
+		displayedSkinParts, _ := packet.ReadBytes(1)
+		mainHand, _ := packet.ReadVarInt()
+		enableTextFiltering, _ := packet.ReadBoolean()
+		allowServerListing, _ := packet.ReadBoolean()
+		fmt.Println("[ConfSettings]", "id:", packet.id, "locale:", locale, "viewDistance:", viewDistance, "chatMode:", chatMode, "chatColor:", charColors, "displayedSkinParts:", displayedSkinParts, "mainHand:", mainHand, "enableTextFiltering:", enableTextFiltering, "allowServerListing:", allowServerListing)
+
+		outPacket := &outgouingPacket{
+			id: 0x03,
+		}
+
+		conn.SendPacket(*outPacket)
+
 	case 0x02:
 
-		channelLength, _ := packet.ReadVarInt()
-		channel, _ := packet.ReadBytes(channelLength)
-		data, _ := packet.ReadBytes(packet.length - len(helper.WriteVarInt(channelLength)) - channelLength)
+		channel := packet.ReadString()
+		data, _ := packet.ReadBytes(packet.length - packet.offset)
 
 		fmt.Println("[ConfCustomPayload]", "id:", packet.id, "channel:", string(channel), "data:", string(data), "length:", len(data))
 
-	case 0x05:
-
-		// id, _ := packet.ReadInt()
-
-		fmt.Println("[ConfPong]", "id:", packet.id)
+	case 0x03:
+		// https://minecraft.wiki/w/Minecraft_Wiki:Projects/wiki.vg_merge/Protocol#Configuration_Acknowledged
+		// This packet switches the connection state to play.
+		conn.state = helper.Play
+		fmt.Printf("[ConfAck] id: %d\n", packet.id)
 
 	default:
 		fmt.Println("Unknown packet ID in Configuration state:", packet.id)
