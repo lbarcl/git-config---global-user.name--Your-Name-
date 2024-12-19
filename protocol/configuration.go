463157172aa20c95355f36f7d96555e1a4896b6c
Barçın <ersoybarcin@gmail.com>
2024-12-19 09:35:01 +0300

    feat: known packets

--- protocol/configuration.go
+++ protocol/configuration.go
@@ -18,12 +18,7 @@ func HandleConfiguration(conn *connection, packet *incommingPacket) {
 		allowServerListing, _ := packet.ReadBoolean()
 		fmt.Println("[ConfSettings]", "id:", packet.id, "locale:", locale, "viewDistance:", viewDistance, "chatMode:", chatMode, "chatColor:", charColors, "displayedSkinParts:", displayedSkinParts, "mainHand:", mainHand, "enableTextFiltering:", enableTextFiltering, "allowServerListing:", allowServerListing)
 
-		outPacket := &outgouingPacket{
-			id: 0x03,
-		}
-
-		conn.SendPacket(*outPacket)
-
+		SendKnownPacks(conn)
 	case 0x02:
 
 		channel := packet.ReadString()
@@ -37,7 +32,36 @@ func HandleConfiguration(conn *connection, packet *incommingPacket) {
 		conn.state = helper.Play
 		fmt.Printf("[ConfAck] id: %d\n", packet.id)
 
+	case 0x07:
+		packCount, _ := packet.ReadVarInt()
+
+		for i := 1; i > int(packCount); i++ {
+			namespace := packet.ReadString()
+			ID := packet.ReadString()
+			vers := packet.ReadString()
+
+			fmt.Printf("%d -> %s, %s, %s", i, namespace, ID, vers)
+		}
+
 	default:
 		fmt.Println("Unknown packet ID in Configuration state:", packet.id)
 	}
+}
+
+func SendKnownPacks(conn *connection) {
+	packet := outgouingPacket{
+		id: 0x0E,
+	}
+
+	packet.WriteVarInt(0)
+
+	conn.SendPacket(packet)
+}
+
+func SendFinish(conn *connection) {
+	packet := outgouingPacket{
+		id: 0x03,
+	}
+
+	conn.SendPacket(packet)
 }
